224240bee2fe17c1ab7af357f601482d31cf24a2
skelleyboy <dannyskelley@gmail.com>
2024-07-27 19:28:45 -0400

    a
    a

--- src/_data/client.js
+++ src/_data/client.js
@@ -4,12 +4,12 @@ module.exports = {
     "phoneForTel": "985-258-9964",
     "phoneFormatted": "985-258-9964",
     "address": {
-        "lineOne": "1205 Maple Ave",
+        "lineOne": "",
         "lineTwo": "",
-        "city": "Lisle",
-        "state": "IL",
-        "zip": "60532",
-        "mapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2974.9660365507325!2d-88.0751324!3d41.785945600000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880e5120cc03adc1%3A0x9cdb5fe6f3de2b89!2s1205%20Maple%20Ave%2C%20Lisle%2C%20IL%2060532!5e0!3m2!1sen!2sus!4v1718991268043!5m2!1sen!2sus"
+        "city": "",
+        "state": "",
+        "zip": "",
+        "mapLink": "ttps://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5790071.59863152!2d-91.55348015056866!3d44.878916589510155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4d4caa3dc7ca0411%3A0x97dd48597a62c9b3!2sMichigan!5e0!3m2!1sen!2sus!4v1722122873369!5m2!1sen!2sus"
     },
     "socials": {
         "facebook": "https://www.facebook.com/",
