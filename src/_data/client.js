d054f0716b1a42d5a1622230350de0afa6a962a2
skelleyboy <dannyskelley@gmail.com>
2024-07-27 18:52:57 -0400

    a
    a

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,8 +1,8 @@
 module.exports = {
-    "name": "Tomato Tattoo",
+    "name": "Harry's Painting and Residential Worx",
     "email": "info@tomatotattoo.com",
-    "phoneForTel": "(630) 241-0669",
-    "phoneFormatted": "(630) 241-0669",
+    "phoneForTel": "985-258-9964",
+    "phoneFormatted": "985-258-9964",
     "address": {
         "lineOne": "1205 Maple Ave",
         "lineTwo": "",
