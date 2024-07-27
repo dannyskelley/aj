283e0a862f8d46398a11629dd1f26d3d3b64accb
skelleyboy <dannyskelley@gmail.com>
2024-07-27 19:31:13 -0400

    a
    a

--- src/_data/client.js
+++ src/_data/client.js
@@ -9,7 +9,7 @@ module.exports = {
         "city": "",
         "state": "",
         "zip": "",
-        "mapLink": "ttps://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5790071.59863152!2d-91.55348015056866!3d44.878916589510155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4d4caa3dc7ca0411%3A0x97dd48597a62c9b3!2sMichigan!5e0!3m2!1sen!2sus!4v1722122873369!5m2!1sen!2sus"
+        "mapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5790071.59863152!2d-91.55348015056866!3d44.878916589510155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4d4caa3dc7ca0411%3A0x97dd48597a62c9b3!2sMichigan!5e0!3m2!1sen!2sus!4v1722122873369!5m2!1sen!2sus"
     },
     "socials": {
         "facebook": "https://www.facebook.com/",
